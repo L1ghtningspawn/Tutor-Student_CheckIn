9986869c5222b871936a9359ad8481581a19e138
Christopher Garcia <60279080+garcichr@users.noreply.github.com>
2020-03-24 22:58:19 -0400

    Student email check-in from tutor mode

--- android/app/src/main/java/kean20sp/capstoneproject/CheckIn_Activity.java
+++ android/app/src/main/java/kean20sp/capstoneproject/CheckIn_Activity.java
@@ -1,5 +1,6 @@
 package kean20sp.capstoneproject;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
@@ -20,6 +21,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import kean20sp.capstoneproject.http.CheckinHandler;
 import kean20sp.capstoneproject.http.TutorCourseListHandler;
 import kean20sp.capstoneproject.util.AppState;
 import kean20sp.capstoneproject.util.QRUtil;
@@ -97,19 +99,40 @@ public class CheckIn_Activity extends AppCompatActivity {
         } else {
             Toast.makeText(this,result, Toast.LENGTH_SHORT).show();
         }
-        List<String> list_courses = Arrays.asList(courses);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list_courses);
+//        List<String> list_courses = Arrays.asList(courses);
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list_courses);
+//
+//        //courselist_sp.setAdapter(adapter);
+//        try{
+//            String qrcode_value = QRUtil.genkey();
+//            QRGEncoder qrgEncoder = new QRGEncoder(qrcode_value,null,QRGContents.Type.TEXT,2) ;
+//            Bitmap bitmap = qrgEncoder.encodeAsBitmap();
+//            ImageView img = (ImageView) findViewById(R.id.qrcode);
+//            img.setImageBitmap(bitmap);
+//        } catch(Exception e){
+//            e.printStackTrace();
+//        }
 
-        //courselist_sp.setAdapter(adapter);
-        try{
-            String qrcode_value = QRUtil.genkey();
-            QRGEncoder qrgEncoder = new QRGEncoder(qrcode_value,null,QRGContents.Type.TEXT,2) ;
-            Bitmap bitmap = qrgEncoder.encodeAsBitmap();
-            ImageView img = (ImageView) findViewById(R.id.qrcode);
-            img.setImageBitmap(bitmap);
-        } catch(Exception e){
-            e.printStackTrace();
-        }
+        checkin_tv.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                CheckinHandler chandler = new CheckinHandler();
+                String result = chandler.checkin(email,email_tv.getText().toString());
+                if(result.equals(CheckinHandler.CHECKIN_SUCCESSFUL)) {
+                    Intent it = new Intent(CheckIn_Activity.this, Student_Checked_In_Activity.class);
+//                    AppState.Session.id = session_id;
+//                    AppState.UserInfo.email = email;
+                    startActivity(it);
+                } else if(result.equals(CheckinHandler.INVALID_SESSION)){
+                    Toast.makeText(CheckIn_Activity.this,result,Toast.LENGTH_LONG).show();
+                } else if(result.equals(CheckinHandler.TUTOR_SESSION_EXISTS)){
+                    Toast.makeText(CheckIn_Activity.this,result,Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(CheckIn_Activity.this,result,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
---
+++ android/app/src/main/java/kean20sp/capstoneproject/http/CheckinHandler.java
@@ -0,0 +1,83 @@
+package kean20sp.capstoneproject.http;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.NameValuePair;
+import cz.msebera.android.httpclient.message.BasicNameValuePair;
+import kean20sp.capstoneproject.util.AppState;
+
+public class CheckinHandler extends HTTPConnectionHandler {
+    private String host = null;
+    private String filepath = null;
+    private String session_id = AppState.Session.id;
+    private ArrayList<NameValuePair> pairs;
+
+    public CheckinHandler(){
+        host = "seesselm-project-page.com";
+        filepath = "/Capstone/android/android_student_checkin.php";
+    }
+    public CheckinHandler(String host, String filepath){
+        this.host = host;
+        this.filepath = filepath;
+        this.session_id = session_id();
+    }
+
+    public String session_id(){
+        return session_id;
+    }
+
+    private String response;
+    private boolean response_done = false;
+    public String checkin(String tutor_email, String student_email){
+        pairs = new ArrayList<NameValuePair>();
+        pairs.add(new BasicNameValuePair("tutor_email",tutor_email));
+        pairs.add(new BasicNameValuePair("student_email",student_email));
+        pairs.add(new BasicNameValuePair("session_id",this.session_id));
+        pairs.add(new BasicNameValuePair("checkin_type","email"));
+
+        Thread t = new Thread(new Runnable(){
+            public void run(){
+                try{
+                    response = makeRequest(host,filepath,pairs);
+                }catch(Exception e){
+                    System.out.println(e);
+                }finally {
+                    response_done = true;
+                    System.out.println(response);
+                }
+            }
+        });
+
+        t.start();
+
+        long tlim = 1000*2;
+        long start = System.currentTimeMillis();
+        while(!response_done & (System.currentTimeMillis() - start) <= tlim){}
+        response_done = false;
+
+        return process(response);
+    }
+
+    private String process(String response) {
+        if(response==null){
+            return "null";
+        }
+        String[] response_split = response.split(";");
+        String code = response_split[0];
+        if(code.equals("SC0")){
+            return CHECKIN_SUCCESSFUL;
+        } else if(code.equals("FCO")){
+            return INVALID_SESSION;
+        } else if(code.equals("FC1")){
+            return TUTOR_SESSION_EXISTS;
+        } else {
+            return CHECKIN_FAILED;
+        }
+    }
+
+    public static final String INVALID_SESSION = "Login Session is invalid. Login Again.";
+    public static final String TUTOR_SESSION_EXISTS = "You are already in an active session.";
+    public static final String CHECKIN_FAILED = "Failed to check-in. Try again.";
+    public static final String CHECKIN_SUCCESSFUL = "Check-in was successful.";
+
+}
