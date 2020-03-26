dc891011b26c40dcf94d68a7989cea34f551f6ba
Christopher Garcia <60279080+garcichr@users.noreply.github.com>
2020-03-25 21:27:54 -0400

    Scripts and test pages updated.

--- php/android_php/android_tutor_checkin.php
+++ php/android_php/android_tutor_checkin.php
@@ -6,10 +6,15 @@
 	if (isset($_POST['session_id'])){
 		$current_session_id = $_POST['session_id'];
 	}
-	if (isset($_POST['checkin_mode'])){
-		$mode = $_POST['checkin_mode'];
+	if(isset($_POST['checkin_type'])){
+		$checkin_type = $_POST['checkin_type'];
 	}
-	if ($mode == 'tutor'){
+
+	if ($checkin_type == 'qrcode'){
+		//TODO
+		//check if qrcode doesn't already exist, if it does: return failure code "old_qrcode"
+		//otherwise return success code
+	} else{
 		$tutor_email = $_POST['tutor_email'];
 		$student_email = $_POST['student_email'];
 
@@ -67,10 +72,11 @@
 			$query3 =
 			'select count(ts_id)
 			from TUTOR_SESSION 
-			where s_ur_id = ?
+			where t_ur_id = ?
+			and s_ur_id = ?
 			and time_out = \'00:00:00\';';
 			$stmt = $con->prepare($query3);
-			$stmt->bind_param('i', $sid);
+			$stmt->bind_param('ii', $tid,$sid);
 			$stmt->bind_result($ts_count);
 			$stmt->execute();
 			$stmt->fetch();
@@ -82,86 +88,20 @@
 				$stmt = $con->prepare($query4);
 				$stmt->bind_param('ii', $tid,$sid);
 				$stmt->execute();
+
 				$row_count = $stmt->affected_rows;
-				$stmt->close();
 
 				if ($row_count == 0){
 					echo "FC2";
 				} else {
 					echo "SC0";
 				}
+				$stmt->close();
 			} else{
 				echo "FC1";
 			}
 		} else {
 			echo "FC0";
-		}
-	} else{
-		echo "STUDENT_MODE!\n";
-		// checkin mode is student entry
-		$student_email = $_POST['email'];
-		echo "STUDENT_EMAIL: {$student_email}\n";
-
-		if(isset($_POST['checkin_type'])){
-			$checkin_type = $_POST['checkin_type'];
-		}
-		if ($checkin_type == 'qrcode'){
-		//TODO
-		//check if qrcode doesn't already exist, if it does: return failure code "old_qrcode"
-		//otherwise return success code
-		} else{
-			if (isset($_POST['checkin_time'])){
-				$checkin_time = $_POST['checkin_time'];
-			}
-			$s_query =
-			'select ur.ur_id from USER_ROLES as ur
-			join ROLES as r on r.r_id = ur.r_id
-			join USER_INFO as ui on ui.u_id = ur.u_id
-			join LOGIN as l on l.login_id = ui.login_id
-			and r.role_name = \'STUDENT\'
-			and l.email = ?;';
-			$stmt = $con->prepare($s_query);
-			$stmt->bind_param('s', $student_email);
-			$stmt->bind_result($sid);
-			$stmt->execute();
-			$stmt->fetch();
-			$stmt->store_result();
-			$stmt->close();
-			echo "STUDENT_ID: {$sid}\n";
-
-			//check for s_ur_id have active session (time_out=00:00:00) and update row
-			$s_query1 =
-			'select ts_id
-			from TUTOR_SESSION 
-			where s_ur_id = ?
-			and time_out = \'00:00:00\';';
-			$stmt = $con->prepare($s_query1);
-			$stmt->bind_param('i', $sid);
-			$stmt->bind_result($ts_id);
-			$stmt->execute();
-			$stmt->fetch();
-			$stmt->store_result();
-			$stmt->close();
-			echo "TUTOR_SESSION_ID: {$ts_id}\n";
-
-			if($ts_id){
-				$s_query2 = 
-				'update TUTOR_SESSION set time_in=?
-				where ts_id = ?;';
-				$stmt = $con->prepare($s_query2);
-				$stmt->bind_param('si', $checkin_time, $ts_id);
-				$stmt->execute();
-				$row_count = $stmt->affected_rows;
-				$stmt->close();
-
-				if ($row_count == 0){
-					echo "FCE0 - CHECKIN_FAILED";
-				} else {
-					echo "SCE0 - CHECKIN_SUCCESSFUL";
-				}
-			} else{
-				echo "FCE1 - NO_VALID_SESSION_EXISTS";
-			}
 		}
 	}
 ?>
