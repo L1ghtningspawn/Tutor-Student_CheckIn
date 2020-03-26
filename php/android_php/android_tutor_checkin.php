0ef9c192aaded1615b59869d3c6243e64a4dff0b
Christopher Garcia <60279080+garcichr@users.noreply.github.com>
2020-03-26 13:14:56 -0400

    Removing unnecessary check for active mobile session.
    Robert going to handle this at the app startup for active session.

--- php/android_php/android_tutor_checkin.php
+++ php/android_php/android_tutor_checkin.php
@@ -3,9 +3,6 @@
 	include "dbconfig.php";
 	include "password.php";
 
-	if (isset($_POST['session_id'])){
-		$current_session_id = $_POST['session_id'];
-	}
 	if(isset($_POST['checkin_type'])){
 		$checkin_type = $_POST['checkin_type'];
 	}
@@ -18,90 +15,71 @@
 		$tutor_email = $_POST['tutor_email'];
 		$student_email = $_POST['student_email'];
 
-		//is there an existing session?
-		$query =
-		'select session_id
-		from MOBILE_SESSION_DATA
-		where login_id=(select login_id from LOGIN where email=?);';
-		$stmt = $con->prepare($query);
-		$stmt->bind_param("s",$tutor_email);
-		$stmt->bind_result($session_id);
+		// here use tutor and student email and check in student to session
+		$query1 =
+		'select ur.ur_id from USER_ROLES as ur
+		join ROLES as r on r.r_id = ur.r_id
+		join USER_INFO as ui on ui.u_id = ur.u_id
+		join LOGIN as l on l.login_id = ui.login_id
+		and r.role_name = \'TUTOR\'
+		and l.email = ?;';
+		$stmt = $con->prepare($query1);
+		$stmt->bind_param('s', $tutor_email);
+		$stmt->bind_result($tid);
+		$stmt->execute();
+		$stmt->fetch();
+		$stmt->store_result();
+		$stmt->close();
+		//echo "TUTOR_ID: {$tid}\n";
+
+		$query2 =
+		'select ur.ur_id from USER_ROLES as ur
+		join ROLES as r on r.r_id = ur.r_id
+		join USER_INFO as ui on ui.u_id = ur.u_id
+		join LOGIN as l on l.login_id = ui.login_id
+		and r.role_name = \'STUDENT\'
+		and l.email = ?;';
+		$stmt = $con->prepare($query2);
+		$stmt->bind_param('s', $student_email);
+		$stmt->bind_result($sid);
+		$stmt->execute();
+		$stmt->fetch();
+		$stmt->store_result();
+		$stmt->close();
+		//echo "STUDENT_ID: {$sid}\n";
+
+		//check to make sure t_ur_id and s_ur_id don't have active sessions (time_out=00:00:00)
+		//make initial insert with c_id of 4 for DUMMY course
+		$query3 =
+		'select count(ts_id)
+		from TUTOR_SESSION 
+		where t_ur_id = ?
+		and s_ur_id = ?
+		and time_out = \'00:00:00\';';
+		$stmt = $con->prepare($query3);
+		$stmt->bind_param('ii', $tid,$sid);
+		$stmt->bind_result($ts_count);
 		$stmt->execute();
 		$stmt->fetch();
 		$stmt->store_result();
 		$stmt->close();
 
-		//echo "{$tutor_email}\n{$student_email}\n{$session_id}\n";
-
-		if(isset($session_id) && $session_id == $current_session_id){
-			// here use tutor and student email and check in student to session
-			$query1 =
-			'select ur.ur_id from USER_ROLES as ur
-			join ROLES as r on r.r_id = ur.r_id
-			join USER_INFO as ui on ui.u_id = ur.u_id
-			join LOGIN as l on l.login_id = ui.login_id
-			and r.role_name = \'TUTOR\'
-			and l.email = ?;';
-			$stmt = $con->prepare($query1);
-			$stmt->bind_param('s', $tutor_email);
-			$stmt->bind_result($tid);
-			$stmt->execute();
-			$stmt->fetch();
-			$stmt->store_result();
-			$stmt->close();
-			//echo "TUTOR_ID: {$tid}\n";
-
-			$query2 =
-			'select ur.ur_id from USER_ROLES as ur
-			join ROLES as r on r.r_id = ur.r_id
-			join USER_INFO as ui on ui.u_id = ur.u_id
-			join LOGIN as l on l.login_id = ui.login_id
-			and r.role_name = \'STUDENT\'
-			and l.email = ?;';
-			$stmt = $con->prepare($query2);
-			$stmt->bind_param('s', $student_email);
-			$stmt->bind_result($sid);
-			$stmt->execute();
-			$stmt->fetch();
-			$stmt->store_result();
-			$stmt->close();
-			//echo "STUDENT_ID: {$sid}\n";
-
-			//check to make sure t_ur_id and s_ur_id don't have active sessions (time_out=00:00:00)
-			//make initial insert with c_id of 4 for DUMMY course
-			$query3 =
-			'select count(ts_id)
-			from TUTOR_SESSION 
-			where t_ur_id = ?
-			and s_ur_id = ?
-			and time_out = \'00:00:00\';';
-			$stmt = $con->prepare($query3);
+		if($ts_count == 0){
+			$query4 = 'insert into TUTOR_SESSION (t_ur_id, s_ur_id, c_id) values (?,?,4);';
+			$stmt = $con->prepare($query4);
 			$stmt->bind_param('ii', $tid,$sid);
-			$stmt->bind_result($ts_count);
 			$stmt->execute();
-			$stmt->fetch();
-			$stmt->store_result();
-			$stmt->close();
 
-			if($ts_count == 0){
-				$query4 = 'insert into TUTOR_SESSION (t_ur_id, s_ur_id, c_id) values (?,?,4);';
-				$stmt = $con->prepare($query4);
-				$stmt->bind_param('ii', $tid,$sid);
-				$stmt->execute();
+			$row_count = $stmt->affected_rows;
 
-				$row_count = $stmt->affected_rows;
-
-				if ($row_count == 0){
-					echo "FC2";
-				} else {
-					echo "SC0";
-				}
-				$stmt->close();
-			} else{
-				echo "FC1";
+			if ($row_count == 0){
+				echo "FC2";
+			} else {
+				echo "SC0";
 			}
-		} else {
-			echo "FC0";
+			$stmt->close();
+		} else{
+			echo "FC1";
 		}
 	}
 ?>
