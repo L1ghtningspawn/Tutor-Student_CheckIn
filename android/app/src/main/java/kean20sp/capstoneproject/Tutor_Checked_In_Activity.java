33237b883c9a88ec36e3f8825b9582ecf654fa70
Robert Kulesza <kuleszar@kean.edu>
2020-03-31 09:07:56 -0400

    update from development directory

--- android/app/src/main/java/kean20sp/capstoneproject/Tutor_Checked_In_Activity.java
+++ android/app/src/main/java/kean20sp/capstoneproject/Tutor_Checked_In_Activity.java
@@ -3,6 +3,7 @@ package kean20sp.capstoneproject;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class Tutor_Checked_In_Activity extends AppCompatActivity {
 
@@ -10,6 +11,14 @@ public class Tutor_Checked_In_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor__checked__in_);
+
+        TextView end_session_button = findViewById(R.id.check_out);
+        end_session_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 
