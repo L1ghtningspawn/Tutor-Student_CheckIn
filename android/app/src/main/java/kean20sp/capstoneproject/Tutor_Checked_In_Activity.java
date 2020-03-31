0b2e32b9ed2ce5a960f8c67639651c5e508e03b4
L1ghtningspawn <34194666+L1ghtningspawn@users.noreply.github.com>
2020-03-30 21:22:36 -0400

    gfds

--- android/app/src/main/java/kean20sp/capstoneproject/Tutor_Checked_In_Activity.java
+++ android/app/src/main/java/kean20sp/capstoneproject/Tutor_Checked_In_Activity.java
@@ -3,6 +3,9 @@ package kean20sp.capstoneproject;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+
+import kean20sp.capstoneproject.util.AppState;
 
 public class Tutor_Checked_In_Activity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class Tutor_Checked_In_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor__checked__in_);
 
+        //R.id.check_out refers to xml
+        TextView end_session = (TextView)findViewById(R.id.check_out);
+        end_session.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 
 }
