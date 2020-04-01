35b80846d6db43863c3103d23d06ad6666469101
L1ghtningspawn <34194666+L1ghtningspawn@users.noreply.github.com>
2020-04-01 08:39:00 -0400

    cool

--- android/app/src/main/java/kean20sp/capstoneproject/http/TutorEndAllSessions.java
+++ android/app/src/main/java/kean20sp/capstoneproject/http/TutorEndAllSessions.java
@@ -6,33 +6,32 @@ import java.util.List;
 import cz.msebera.android.httpclient.NameValuePair;
 import cz.msebera.android.httpclient.message.BasicNameValuePair;
 
-public class TutorEndSessionHandler extends HTTPConnectionHandler{
+public class TutorEndAllSessions  extends HTTPConnectionHandler{
     private String host = null, filepath = null;
 
-    public TutorEndSessionHandler(String host, String filepath){
+    public TutorEndAllSessions(String host, String filepath){
         this.host=host;
         this.filepath=filepath;
     }
 
-    public TutorEndSessionHandler(){
+    public TutorEndAllSessions(){
         host = "seesselm-project-page.com";
-        filepath = "/Capstone/android/Tutor_Individual_checkout.php";
+        filepath = "/Capstone/android/End_All_Sessions.php";
     }
     private String response;
     private boolean response_done = false;
     private List<NameValuePair> pairs;
-    public String Individual_Session_End(String StudentID, String TutorID){ //why string?
+    public String Individual_Session_End(String StudentID, String TutorID) { //why string?
         pairs = new ArrayList<NameValuePair>();
         pairs.add(new BasicNameValuePair("user_role_id", TutorID));
-        pairs.add(new BasicNameValuePair("student_id", StudentID));
 
-        Thread t = new Thread(new Runnable(){
-            public void run(){
-                try{
-                    response = makeRequest(host,filepath,pairs);
-                }catch(Exception e){
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    response = makeRequest(host, filepath, pairs);
+                } catch (Exception e) {
                     System.out.println(e);
-                }finally {
+                } finally {
                     response_done = true;
                     System.out.println(response);
                 }
@@ -41,9 +40,10 @@ public class TutorEndSessionHandler extends HTTPConnectionHandler{
 
         t.start();
 
-        long tlim = 1000*2;
+        long tlim = 1000 * 2;
         long start = System.currentTimeMillis();
-        while(!response_done & (System.currentTimeMillis() - start) <= tlim){}
+        while (!response_done & (System.currentTimeMillis() - start) <= tlim) {
+        }
         response_done = false;
 
         return process(response);
@@ -51,10 +51,9 @@ public class TutorEndSessionHandler extends HTTPConnectionHandler{
 
     private String process(String response) {
 
-        if(response.equals("S01")){
+        if (response.equals("S01")) {
             return SESSION_END_SUCCESS;
-        }
-        else{
+        } else {
             return SESSION_END_FAILURE;
         }
     }
