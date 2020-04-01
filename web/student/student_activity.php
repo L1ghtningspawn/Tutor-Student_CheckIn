5d540f6e0daa88b76900dddcd0425f8aa9864991
seesselm <36243915+seesselm@users.noreply.github.com>
2020-04-01 10:12:31 -0400

    Update student_activity.php

--- web/student/student_activity.php
+++ web/student/student_activity.php
@@ -4,7 +4,7 @@ include ('../dbconfig.php');
 #$var=$_POST['var_from_ajax'];
 $var=$_SESSION["Student_id"];
 $query="Select ts_id,t_ur_id,s_ur_id,time_in,time_out,c_id from $server_database.TUTOR_SESSION where s_ur_id=$var order by ts_id desc";
-echo $query;
+#echo $query;
 $result=mysqli_query($con,$query);
 $array=[];
 while($row=mysqli_fetch_array($result))
