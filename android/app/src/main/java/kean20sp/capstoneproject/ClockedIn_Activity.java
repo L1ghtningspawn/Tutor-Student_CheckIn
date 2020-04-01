2febc892b8625b7209bc47fb038b0baac3f71686
Robert Kulesza <kuleszar@kean.edu>
2020-04-01 06:37:42 -0400

    Update ClockedIn_Activity.java

--- android/app/src/main/java/kean20sp/capstoneproject/ClockedIn_Activity.java
+++ android/app/src/main/java/kean20sp/capstoneproject/ClockedIn_Activity.java
@@ -22,7 +22,7 @@ public class ClockedIn_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clocked_in_);
 
         AppState.Debug.log_All();
