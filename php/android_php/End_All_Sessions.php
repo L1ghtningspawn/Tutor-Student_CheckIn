64524048b481ff5f2ff8173b6f5e50289f8181eb
rkulesza9 <kuleszar@kean.edu>
2020-04-08 17:53:54 -0400

    Update End_All_Sessions.php

--- php/android_php/End_All_Sessions.php
+++ php/android_php/End_All_Sessions.php
@@ -5,14 +5,13 @@ include "password.php";
 
 //test connection
 if (!$con){
-    die("FAILED" . mysqli_connect_error());
+    die("?" . mysqli_connect_error());
 }
-
 $t_ur_id = $_POST['user_role_id'];
 
-$time_out_query = "update TUTOR_SESSION 
+$time_out_query = "update TUTOR_SESSION
 set time_out = now()
-where time_out is null 
+where time_out is null
 and t_ur_id={$t_ur_id};";
 
 
@@ -20,11 +19,6 @@ $null_checkout_stmt = $con->prepare($time_out_query);
 $null_checkout_stmt->execute();
 $row_count = $con->affected_rows;
 $null_checkout_stmt->close();
-if($row_count>0){
-    echo "S01";
-}
-else{
-    echo "?";
-}
+echo "S01";
 
 ?>
