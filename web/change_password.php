00b0ad233164fec4d73228d02f40d07b7cefa55c
seesselm <36243915+seesselm@users.noreply.github.com>
2020-05-09 13:36:48 -0400

    fixed elevate tutor

--- web/change_password.php
+++ web/change_password.php
@@ -1,34 +1,66 @@
-<?php
-
-include ("../include.php");
-$password_old = mysqli_real_escape_string($con, $_POST['password_old']);
-$password_new = mysqli_real_escape_string($con, $_POST['password_new']);
-$password_new_confirm = mysqli_real_escape_string($con, $_POST['password_new_confirm']);
-
-$id=$_SESSION['u_id'];
-if(strcmp($password_new,$password_new_confirm)==0)
-{
-    $query = "select password from $server_database.LOGIN where login_id=$id";
-    $result = mysqli_query($con, $query);
-    $row=mysqli_fetch_array($result);
-    $hash=$row['password'];
-    if(password_verify($password_old, $hash))
-    {
+<?php
 
-        $pass=password_hash($password_new, PASSWORD_BCRYPT);
-        $query="Update $server_database.LOGIN set password='$pass' where login_id=$id";
-        mysqli_query($con,$query);
-        #header( "Location: WHERE.php" );
-        echo "Successful password update";
-    }
-    else
-    {
-        #header( "refresh:10;url=change_password.php" );
-        echo "incorrect old password";
-    }
-}
-else
-{
-    header( "refresh:10;url=change_password.php" );
-    echo "New password does not match";
+session_start();
+
+include ("include.php");
+
+$password_old = mysqli_real_escape_string($con, $_POST['password_old']);
+
+$password_new = mysqli_real_escape_string($con, $_POST['password_new']);
+
+$password_new_confirm = mysqli_real_escape_string($con, $_POST['password_new_confirm']);
+
+
+
+$id=$_SESSION['u_id'];
+
+if(strcmp($password_new,$password_new_confirm)==0)
+
+{
+
+    $query = "select password from $server_database.LOGIN where login_id=$id";
+
+    $result = mysqli_query($con, $query);
+
+    $row=mysqli_fetch_array($result);
+
+    $hash=$row['password'];
+
+    if(password_verify($password_old, $hash))
+
+    {
+
+
+        $pass=password_hash($password_new, PASSWORD_BCRYPT);
+
+        $query="Update $server_database.LOGIN set password='$pass' where login_id=$id";
+
+        mysqli_query($con,$query);
+
+        #header( "Location: WHERE.php" );
+
+        echo "Successful password update";
+
+    }
+
+    else
+
+    {
+
+        #header( "refresh:10;url=change_password.php" );
+
+        echo "incorrect old password";
+
+    }
+
+}
+
+else
+
+{
+
+    header( "refresh:10;url=change_password.php" );
+
+    echo "New password does not match";
+
 }
--- web/supervisor/new_tutor.php
+++ web/supervisor/new_tutor.php
@@ -7,4 +7,5 @@ mysqli_query($con,$query);
 
 $ur_id = mysqli_insert_id($con);
 $query2="Insert into $server_database.USER_ROLES_DEPARTMENT (ur_id,d_id) values ($ur_id,7)";
+mysqli_query($con,$query2);
 ?>
