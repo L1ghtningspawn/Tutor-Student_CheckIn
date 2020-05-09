5024ea5b311d36cdfec867b375fc1ea3ba911954
seesselm <36243915+seesselm@users.noreply.github.com>
2020-05-09 18:54:28 -0400

    department variables for new tutor

--- web/supervisor/get_non_tutors.php
+++ web/supervisor/get_non_tutors.php
@@ -3,7 +3,12 @@ session_start();
 
 include("../include.php");
 
-$non_tutor="Select ui.fname as fname,ui.lname as lname,ui.year_at_organization as year,ui.u_id as user_id from $server_database.USER_INFO ui where ui.u_id not in (Select u.u_id from $server_database.USER_ROLES u where u.r_id=$tutor)";
+$u_id=$_SESSION['u_id'];
+$department="SELECT d_id FROM $server_database.USER_ROLES_DEPARTMENT where ur_id in (Select ur_id from $server_database.USER_ROLES where r_id=$supervisor and u_id=$u_id)";
+$result=mysqli_query($con,$department);
+$row=mysqli_fetch_assoc($result);
+$d_id=$row['d_id'];
+$non_tutor="Select ui.fname as fname,ui.lname as lname,ui.year_at_organization as year,ui.u_id as user_id from $server_database.USER_INFO ui where ui.u_id not in (Select u.u_id from $server_database.USER_ROLES u where u.r_id=$tutor and u.ur_id in (Select urd.ur_id from $server_database.USER_ROLE_DEPARTMENT where urd.d_id=$d_id))";
 $result=mysqli_query($con,$non_tutor);
 $to_web=[];
 while($row=mysqli_fetch_assoc($result))
--- web/supervisor/new_tutor.php
+++ web/supervisor/new_tutor.php
@@ -5,7 +5,13 @@ $from_web=$_POST['u_id'];
 $query="Insert into $server_database.USER_ROLES (u_id,r_id) values ($from_web,5)";
 mysqli_query($con,$query);
 
+$u_id=$_SESSION['u_id'];
+$department="SELECT d_id FROM $server_database.USER_ROLES_DEPARTMENT where ur_id in (Select ur_id from $server_database.USER_ROLES where r_id=$supervisor and u_id=$u_id)";
+$result=mysqli_query($con,$department);
+$row=mysqli_fetch_assoc($result);
+$d_id=$row['d_id'];
+
 $ur_id = mysqli_insert_id($con);
-$query2="Insert into $server_database.USER_ROLES_DEPARTMENT (ur_id,d_id) values ($ur_id,7)";
+$query2="Insert into $server_database.USER_ROLES_DEPARTMENT (ur_id,d_id) values ($ur_id,$d_id)";
 mysqli_query($con,$query2);
 ?>
