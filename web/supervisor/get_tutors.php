7dc52f0d3b1e8c182f74635ea6cf75c83edbe8d2
seesselm <36243915+seesselm@users.noreply.github.com>
2020-05-09 19:02:51 -0400

    department id get tutors

--- web/supervisor/get_tutors.php
+++ web/supervisor/get_tutors.php
@@ -2,8 +2,14 @@
 session_start();
 
 include("../include.php");
+$u_id=$_SESSION['u_id'];
+$department="SELECT d_id FROM $server_database.USER_ROLES_DEPARTMENT where ur_id in (Select ur_id from $server_database.USER_ROLES where r_id=$supervisor and u_id=$u_id)";
+$result=mysqli_query($con,$department);
+$row=mysqli_fetch_assoc($result);
+$d_id=$row['d_id'];
 
-$tutor="Select ui.fname as fname,ui.lname as lname,ui.year_at_organization as year,ui.u_id as user_id from $server_database.USER_INFO ui where ui.u_id in (Select u.u_id from $server_database.USER_ROLES u where u.r_id=$tutor)";
+#$tutor="Select ui.fname as fname,ui.lname as lname,ui.year_at_organization as year,ui.u_id as user_id from $server_database.USER_INFO ui where ui.u_id in (Select u.u_id from $server_database.USER_ROLES u where u.r_id=$tutor)";
+$tutor="Select fname,lname,year_at_organization as year,u_id as user_id from USER_INFO where u_id in (Select u_id from USER_ROLES where ur_id in (SELECT ur_id FROM seesselm_Capstone.USER_ROLES_DEPARTMENT where d_id=$d_id) and r_id=$tutor)";
 $result=mysqli_query($con,$tutor);
 $to_web=[];
 while($row=mysqli_fetch_assoc($result))
